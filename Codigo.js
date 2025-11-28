6d61b8fd67ea60e042b62432eab59a4f708c7e9b
Lucas G A LIMA <lukinhascop1@gmail.com>
2025-11-28 15:05:35 -0300

    Removendo node_modules do versionamento

--- Codigo.js
+++
@@ -1,6 +0,0 @@
-const soma = (a, b) => a + b;
-const dobro = valor => valor * 2;
-const divisao = (a, b) => a / b;
-const porcentagem = (p, valor) => (p / 100) * valor;
-
-module.exports = { soma, dobro, divisao, porcentagem };
